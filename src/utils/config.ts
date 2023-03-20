6b7d3014cf0b71c1f4e8c6be2227932a42ac6f1a
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-20 20:11:38 -0300

    Add the DB Variables in the config file

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,4 +1,10 @@
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-export const PORT = process.env.PORT || 8080
+export const PORT = process.env.PORT || 8080;
+export const DB = {
+  DB_DATABASE: process.env.DB_DATABASE || "todo_list",
+  DB_USERNAME: process.env.DB_USERNAME || "root",
+  DB_PASSWORD: process.env.DB_PASSWORD || "root",
+  DB_HOST: process.env.DB_HOST || "localhost"
+};
