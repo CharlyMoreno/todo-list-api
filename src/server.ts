d349211cd51abd8ffd8c8678b015b2416a73e126
CharlyMoreno <carlitosmoreno12@hotmail.com>
2023-03-20 20:10:09 -0300

    Add the connection function to Database in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import app from "./app";
 
 import {PORT} from "./utils/config"
 
+import { createConnection } from "./database/connection";
+
+createConnection()
+
 app.listen(PORT, () => {
   console.log("Server on port:", PORT);
 });
